cb471680cbe1e430598ce72632f93f9a4f421d3a
Bams <61815364+BambangJ@users.noreply.github.com>
2024-04-08 00:05:59 +0700

    Update MainActivity.kt
    Minor changes

--- app/src/main/java/com/example/layoutingdigiteam/MainActivity.kt
+++ app/src/main/java/com/example/layoutingdigiteam/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+//07 apr 2024 BambangJayasantika 10121030
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
